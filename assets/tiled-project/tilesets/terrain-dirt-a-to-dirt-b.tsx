3f90bc83ab3d96d177cadfcba77d72c5cbb92680
Max Maldonado <nuup20@gmail.com>
2025-10-29 09:12:34 -0600

    added assets

---
+++ assets/tiled-project/tilesets/terrain-dirt-a-to-dirt-b.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="terrain-dirt-a-to-dirt-b" tilewidth="128" tileheight="64" tilecount="28" columns="4">
+ <image source="../../textures/terrain/128x64 Dirt A to Dirt B.png" width="512" height="448"/>
+</tileset>
---
+++ assets/tiled-project/tilesets/terrain-dirt-a-to-sand-a.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="dirt-a-to-sand-a" tilewidth="128" tileheight="64" tilecount="32" columns="4">
+ <image source="../../textures/terrain/128x64 Dirt A to Sand A.png" width="512" height="512"/>
+ <wangsets>
+  <wangset name="Dirt A to Sand" type="corner" tile="29">
+   <wangcolor name="" color="#ff0000" tile="29" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="1" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="2" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="3" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="16" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="17" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="18" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="19" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="20" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="22" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="24" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="26" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="29" wangid="0,1,0,1,0,1,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
