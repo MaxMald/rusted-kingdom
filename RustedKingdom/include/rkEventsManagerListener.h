e871569d52ebba8e71dc9f6efbdfb1126a6c20a2
Max Maldonado <41883722+MaxMald@users.noreply.github.com>
2025-11-03 19:39:55 -0600

    Update event manager listener

--- RustedKingdom/include/rkEventsManagerListener.h
+++ RustedKingdom/include/rkEventsManagerListener.h
@@ -1,12 +1,26 @@
 #pragma once
 
 #include "rkPrerequisites.h"
+#include "rkEvent.h"
 
 namespace rk
 {
+  /**
+   * @brief Interface for objects that want to receive events from
+   * EventsManager.
+   */
   class EventsManagerListener
   {
   public:
+    /**
+     * @brief Virtual default destructor.
+     */
     virtual ~EventsManagerListener() = default;
+
+    /**
+     * @brief Called by EventsManager when an event is dispatched.
+     * @param event The dispatched event to handle.
+     */
+    virtual void onEvent(const Event& event) = 0;
   };
 }
--- RustedKingdom/src/rkEventsManagerListener.cpp
+++
@@ -1,6 +0,0 @@
-#include "rkEventsManagerListener.h"
-
-namespace rk
-{
-
-}
