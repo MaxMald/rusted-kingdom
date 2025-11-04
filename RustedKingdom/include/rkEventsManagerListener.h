cddcf0b410c399545f2ab5e8da45a4edad93f149
Max Maldonado <41883722+MaxMald@users.noreply.github.com>
2025-11-03 19:27:10 -0600

    Add events manager listener

---
+++ RustedKingdom/include/rkEventsManagerListener.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "rkPrerequisites.h"
+
+namespace rk
+{
+  class EventsManagerListener
+  {
+  public:
+    virtual ~EventsManagerListener() = default;
+  };
+}
---
+++ RustedKingdom/src/rkEventsManagerListener.cpp
@@ -0,0 +1,6 @@
+#include "rkEventsManagerListener.h"
+
+namespace rk
+{
+
+}
