e5953ee01f26981638e9adfa58eff2015a8c4661
Max Maldonado <41883722+MaxMald@users.noreply.github.com>
2025-11-04 10:14:58 -0600

    Add assets

---
+++ assets/tiled-project/tilesets/plants-01.tsx
@@ -0,0 +1,241 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="plants-01" tilewidth="256" tileheight="256" tilecount="79" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../textures/plants/plants-01/bamboo01.png" width="256" height="128"/>
+ </tile>
+ <tile id="1">
+  <image source="../../textures/plants/plants-01/bamboo02.png" width="256" height="128"/>
+ </tile>
+ <tile id="2">
+  <image source="../../textures/plants/plants-01/bamboo03.png" width="256" height="128"/>
+ </tile>
+ <tile id="3">
+  <image source="../../textures/plants/plants-01/bamboo04.png" width="256" height="128"/>
+ </tile>
+ <tile id="4">
+  <image source="../../textures/plants/plants-01/bamboo05.png" width="256" height="128"/>
+ </tile>
+ <tile id="5">
+  <image source="../../textures/plants/plants-01/bamboo06.png" width="256" height="128"/>
+ </tile>
+ <tile id="6">
+  <image source="../../textures/plants/plants-01/bigtree01.png" width="256" height="256"/>
+ </tile>
+ <tile id="7">
+  <image source="../../textures/plants/plants-01/bigtree02.png" width="256" height="256"/>
+ </tile>
+ <tile id="8">
+  <image source="../../textures/plants/plants-01/bigtree03.png" width="256" height="256"/>
+ </tile>
+ <tile id="9">
+  <image source="../../textures/plants/plants-01/bush01.png" width="256" height="128"/>
+ </tile>
+ <tile id="10">
+  <image source="../../textures/plants/plants-01/bush02.png" width="128" height="64"/>
+ </tile>
+ <tile id="11">
+  <image source="../../textures/plants/plants-01/bush03.png" width="128" height="64"/>
+ </tile>
+ <tile id="12">
+  <image source="../../textures/plants/plants-01/bush04.png" width="128" height="64"/>
+ </tile>
+ <tile id="13">
+  <image source="../../textures/plants/plants-01/bush05.png" width="256" height="128"/>
+ </tile>
+ <tile id="14">
+  <image source="../../textures/plants/plants-01/cactus01.png" width="128" height="128"/>
+ </tile>
+ <tile id="15">
+  <image source="../../textures/plants/plants-01/cactus02.png" width="128" height="128"/>
+ </tile>
+ <tile id="16">
+  <image source="../../textures/plants/plants-01/cactus03.png" width="128" height="128"/>
+ </tile>
+ <tile id="17">
+  <image source="../../textures/plants/plants-01/cactus04.png" width="128" height="128"/>
+ </tile>
+ <tile id="18">
+  <image source="../../textures/plants/plants-01/grasses01.png" width="128" height="64"/>
+ </tile>
+ <tile id="19">
+  <image source="../../textures/plants/plants-01/grasses02.png" width="128" height="64"/>
+ </tile>
+ <tile id="20">
+  <image source="../../textures/plants/plants-01/grasses03.png" width="128" height="64"/>
+ </tile>
+ <tile id="21">
+  <image source="../../textures/plants/plants-01/grasses04.png" width="128" height="64"/>
+ </tile>
+ <tile id="22">
+  <image source="../../textures/plants/plants-01/grasses05.png" width="128" height="64"/>
+ </tile>
+ <tile id="23">
+  <image source="../../textures/plants/plants-01/hemp01.png" width="256" height="128"/>
+ </tile>
+ <tile id="24">
+  <image source="../../textures/plants/plants-01/hemp02.png" width="256" height="128"/>
+ </tile>
+ <tile id="25">
+  <image source="../../textures/plants/plants-01/hemp03.png" width="256" height="128"/>
+ </tile>
+ <tile id="26">
+  <image source="../../textures/plants/plants-01/palm01.png" width="256" height="128"/>
+ </tile>
+ <tile id="27">
+  <image source="../../textures/plants/plants-01/palm02.png" width="256" height="128"/>
+ </tile>
+ <tile id="28">
+  <image source="../../textures/plants/plants-01/palm03.png" width="256" height="128"/>
+ </tile>
+ <tile id="29">
+  <image source="../../textures/plants/plants-01/palm04.png" width="256" height="128"/>
+ </tile>
+ <tile id="30">
+  <image source="../../textures/plants/plants-01/palm05.png" width="256" height="128"/>
+ </tile>
+ <tile id="31">
+  <image source="../../textures/plants/plants-01/palm06.png" width="256" height="128"/>
+ </tile>
+ <tile id="32">
+  <image source="../../textures/plants/plants-01/pine-full01.png" width="256" height="256"/>
+ </tile>
+ <tile id="33">
+  <image source="../../textures/plants/plants-01/pine-full02.png" width="256" height="256"/>
+ </tile>
+ <tile id="34">
+  <image source="../../textures/plants/plants-01/pine-full03.png" width="256" height="256"/>
+ </tile>
+ <tile id="35">
+  <image source="../../textures/plants/plants-01/pine-full04.png" width="256" height="256"/>
+ </tile>
+ <tile id="36">
+  <image source="../../textures/plants/plants-01/pine-full05.png" width="256" height="256"/>
+ </tile>
+ <tile id="37">
+  <image source="../../textures/plants/plants-01/pine-full06.png" width="256" height="256"/>
+ </tile>
+ <tile id="38">
+  <image source="../../textures/plants/plants-01/pine-full07.png" width="256" height="256"/>
+ </tile>
+ <tile id="39">
+  <image source="../../textures/plants/plants-01/pine-full08.png" width="256" height="256"/>
+ </tile>
+ <tile id="40">
+  <image source="../../textures/plants/plants-01/pine-half01.png" width="256" height="256"/>
+ </tile>
+ <tile id="41">
+  <image source="../../textures/plants/plants-01/pine-half02.png" width="256" height="256"/>
+ </tile>
+ <tile id="42">
+  <image source="../../textures/plants/plants-01/pine-half03.png" width="256" height="256"/>
+ </tile>
+ <tile id="43">
+  <image source="../../textures/plants/plants-01/pine-half04.png" width="256" height="256"/>
+ </tile>
+ <tile id="44">
+  <image source="../../textures/plants/plants-01/pine-half05.png" width="256" height="256"/>
+ </tile>
+ <tile id="45">
+  <image source="../../textures/plants/plants-01/pine-half06.png" width="256" height="256"/>
+ </tile>
+ <tile id="46">
+  <image source="../../textures/plants/plants-01/pine-half07.png" width="256" height="256"/>
+ </tile>
+ <tile id="47">
+  <image source="../../textures/plants/plants-01/pine-half08.png" width="256" height="256"/>
+ </tile>
+ <tile id="48">
+  <image source="../../textures/plants/plants-01/pine-none01.png" width="256" height="256"/>
+ </tile>
+ <tile id="49">
+  <image source="../../textures/plants/plants-01/pine-none02.png" width="256" height="256"/>
+ </tile>
+ <tile id="50">
+  <image source="../../textures/plants/plants-01/pine-none03.png" width="256" height="256"/>
+ </tile>
+ <tile id="51">
+  <image source="../../textures/plants/plants-01/pine-none04.png" width="256" height="256"/>
+ </tile>
+ <tile id="52">
+  <image source="../../textures/plants/plants-01/pine-none05.png" width="256" height="256"/>
+ </tile>
+ <tile id="53">
+  <image source="../../textures/plants/plants-01/pine-none06.png" width="256" height="256"/>
+ </tile>
+ <tile id="54">
+  <image source="../../textures/plants/plants-01/pine-none07.png" width="256" height="256"/>
+ </tile>
+ <tile id="55">
+  <image source="../../textures/plants/plants-01/pine-none08.png" width="256" height="256"/>
+ </tile>
+ <tile id="56">
+  <image source="../../textures/plants/plants-01/shrub1-01.png" width="256" height="128"/>
+ </tile>
+ <tile id="57">
+  <image source="../../textures/plants/plants-01/shrub1-02.png" width="256" height="128"/>
+ </tile>
+ <tile id="58">
+  <image source="../../textures/plants/plants-01/shrub1-03.png" width="256" height="128"/>
+ </tile>
+ <tile id="59">
+  <image source="../../textures/plants/plants-01/shrub1-04.png" width="256" height="128"/>
+ </tile>
+ <tile id="60">
+  <image source="../../textures/plants/plants-01/shrub1-05.png" width="256" height="128"/>
+ </tile>
+ <tile id="61">
+  <image source="../../textures/plants/plants-01/shrub2-01.png" width="256" height="128"/>
+ </tile>
+ <tile id="62">
+  <image source="../../textures/plants/plants-01/shrub2-02.png" width="256" height="128"/>
+ </tile>
+ <tile id="63">
+  <image source="../../textures/plants/plants-01/shrub2-03.png" width="256" height="128"/>
+ </tile>
+ <tile id="64">
+  <image source="../../textures/plants/plants-01/shrub2-04.png" width="256" height="128"/>
+ </tile>
+ <tile id="65">
+  <image source="../../textures/plants/plants-01/shrub2-05.png" width="256" height="128"/>
+ </tile>
+ <tile id="66">
+  <image source="../../textures/plants/plants-01/swirl01.png" width="128" height="64"/>
+ </tile>
+ <tile id="67">
+  <image source="../../textures/plants/plants-01/swirl02.png" width="128" height="64"/>
+ </tile>
+ <tile id="68">
+  <image source="../../textures/plants/plants-01/tropical01.png" width="256" height="128"/>
+ </tile>
+ <tile id="69">
+  <image source="../../textures/plants/plants-01/tropical02.png" width="256" height="128"/>
+ </tile>
+ <tile id="70">
+  <image source="../../textures/plants/plants-01/tropical03.png" width="256" height="128"/>
+ </tile>
+ <tile id="71">
+  <image source="../../textures/plants/plants-01/tropical04.png" width="256" height="128"/>
+ </tile>
+ <tile id="72">
+  <image source="../../textures/plants/plants-01/tropical05.png" width="256" height="128"/>
+ </tile>
+ <tile id="73">
+  <image source="../../textures/plants/plants-01/weed01.png" width="128" height="64"/>
+ </tile>
+ <tile id="74">
+  <image source="../../textures/plants/plants-01/weed02.png" width="128" height="64"/>
+ </tile>
+ <tile id="75">
+  <image source="../../textures/plants/plants-01/weed03.png" width="128" height="64"/>
+ </tile>
+ <tile id="76">
+  <image source="../../textures/plants/plants-01/weed04.png" width="128" height="64"/>
+ </tile>
+ <tile id="77">
+  <image source="../../textures/plants/plants-01/weed05.png" width="128" height="64"/>
+ </tile>
+ <tile id="78">
+  <image source="../../textures/plants/plants-01/weed06.png" width="128" height="64"/>
+ </tile>
+</tileset>
