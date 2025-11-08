79bfecbe0d90da3a57a88c8b2d6d025dac90a96f
Max Maldonado <41883722+MaxMald@users.noreply.github.com>
2025-11-08 11:11:27 -0600

    8 Dir Animations

---
+++ RustedKingdom/rkEightDirectionsSpriteSheetAnimationDescription.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#include "rkPrerequisites.h"
+
+namespace rk
+{
+  class EightDirectionsSpriteSheetAnimationDescription
+  {
+  public:
+
+    EightDirectionsSpriteSheetAnimationDescription(
+      const String& animationKey,
+      const String& textureKey,
+      UInt32 frameWidth,
+      UInt32 frameHeight,
+      UInt32 animationLength,
+      float speed
+    ) :
+      m_animationKey(animationKey),
+      m_textureKey(textureKey),
+      m_frameWidth(frameWidth),
+      m_frameHeight(frameHeight),
+      m_animationLength(animationLength),
+      m_speed(speed)
+    {
+    }
+
+    EightDirectionsSpriteSheetAnimationDescription(
+      const EightDirectionsSpriteSheetAnimationDescription& other
+    ) : 
+      m_animationKey(other.m_animationKey),
+      m_textureKey(other.m_textureKey),
+      m_frameWidth(other.m_frameWidth),
+      m_frameHeight(other.m_frameHeight),
+      m_animationLength(other.m_animationLength),
+      m_speed(other.m_speed)
+    {
+    }
+
+    EightDirectionsSpriteSheetAnimationDescription(
+      EightDirectionsSpriteSheetAnimationDescription&& other
+    ) noexcept
+    {
+      m_animationKey = std::move(other.m_animationKey);
+      m_textureKey =  std::move(other.m_textureKey);
+      m_frameWidth = other.m_frameWidth;
+      m_frameHeight = other.m_frameHeight;
+      m_animationLength = other.m_animationLength;
+      m_speed = other.m_speed;
+    }
+
+    ~EightDirectionsSpriteSheetAnimationDescription() = default;
+
+    EightDirectionsSpriteSheetAnimationDescription& operator=(
+      const EightDirectionsSpriteSheetAnimationDescription& other
+      )
+    {
+      if (this != &other)
+      {
+        m_animationKey = other.m_animationKey;
+        m_textureKey = other.m_textureKey;
+        m_frameWidth = other.m_frameWidth;
+        m_frameHeight = other.m_frameHeight;
+        m_animationLength = other.m_animationLength;
+        m_speed = other.m_speed;
+      }
+      return *this;
+    }
+
+    EightDirectionsSpriteSheetAnimationDescription& operator=(
+      EightDirectionsSpriteSheetAnimationDescription&& other
+      ) noexcept
+    {
+      if (this != &other)
+      {
+        m_animationKey = std::move(other.m_animationKey);
+        m_textureKey = std::move(other.m_textureKey);
+        m_frameWidth = other.m_frameWidth;
+        m_frameHeight = other.m_frameHeight;
+        m_animationLength = other.m_animationLength;
+        m_speed = other.m_speed;
+      }
+      return *this;
+    }
+
+    const String& getAnimationKey() const { return m_animationKey; }
+    const String& getTextureKey() const { return m_textureKey; }
+    UInt32 getFrameWidth() const { return m_frameWidth; }
+    UInt32 getFrameHeight() const { return m_frameHeight; }
+    UInt32 getAnimationLength() const { return m_animationLength; }
+    float getSpeed() const { return m_speed; }
+
+  private:
+    String m_animationKey;
+    String m_textureKey;
+    UInt32 m_frameWidth;
+    UInt32 m_frameHeight;
+    UInt32 m_animationLength;
+    float m_speed;
+  };
+}
