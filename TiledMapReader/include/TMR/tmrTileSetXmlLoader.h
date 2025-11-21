03b5d4b632396972cc25f39bed2595854377817a
Max Maldonado <nuup20@gmail.com>
2025-11-21 11:39:28 -0600

    Add Tile Set XML loader

---
+++ TiledMapReader/include/TMR/tmrTileSetXmlLoader.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "TMR/tmrPrerequisites.h"
+#include "TMR/tinyxml2.h"
+
+using namespace tinyxml2;
+
+using std::uint32_t;
+using std::int32_t;
+using std::size_t;
+
+namespace tmr
+{
+  class TileSet;
+  class TileSetTile;
+  class TileSetGrid;
+
+  namespace tileSetXmlLoader
+  {
+    TileSet* loadFromFile(const char* filePath, const int32_t firstGid);
+    TileSet* parseSpriteSheetTileSet(XMLElement* tilesetElement, const int32_t firstGid);
+    TileSet* parseImageCollectionTileSet(XMLElement* tilesetElement, const int32_t firstGid);
+    TileSetGrid* parseTileSetGrid(XMLElement* gridElement);
+    TileSetTile** parseTileSetTileArray(XMLElement* tilesetElement, size_t tileCount);
+    TileSetTile* parseTileSetTile(XMLElement* tileElement);
+  };
+}
---
+++ TiledMapReader/src/tmrTileSetXmlLoader.cpp
@@ -0,0 +1,177 @@
+#include "TMR/tmrTileSetXmlLoader.h"
+
+#include <string>
+
+using std::string;
+
+#include "TMR/tmrOrientation.h"
+#include "TMR/tmrOrientationParser.h"
+#include "TMR/tmrTileSetGrid.h"
+#include "TMR/tmrTileSetTile.h"
+#include "TMR/tmrImageCollectionTileSet.h"
+#include "TMR/tmrSpriteSheetTileSet.h"
+
+namespace tmr
+{
+  namespace tileSetXmlLoader
+  {
+    TileSet* loadFromFile(const char* filePath, const int32_t firstGid)
+    {
+      XMLDocument doc;
+      XMLError loadResult = doc.LoadFile(filePath);
+      if (loadResult != XML_SUCCESS)
+        return nullptr;
+
+      XMLElement* tilesetElement = doc.FirstChildElement("tileset");
+      if (!tilesetElement)
+        return nullptr;
+
+      XMLElement* gridElement = tilesetElement->FirstChildElement("grid");
+      if (gridElement)
+        return parseImageCollectionTileSet(tilesetElement, firstGid);
+
+      XMLElement* imageElement = tilesetElement->FirstChildElement("image");
+      if (imageElement)
+        return parseSpriteSheetTileSet(tilesetElement, firstGid);
+
+      return nullptr;
+    }
+
+    TileSet* parseSpriteSheetTileSet(
+      XMLElement* tilesetElement,
+      const int32_t firstGid
+    )
+    {
+      int32_t margin = tilesetElement->IntAttribute("margin", 0);
+      uint32_t spacing = tilesetElement->UnsignedAttribute("spacing", 0);
+      uint32_t columns = tilesetElement->UnsignedAttribute("columns", 0);
+      uint32_t tileCount = tilesetElement->UnsignedAttribute("tilecount", 0);
+      uint32_t tileHeight = tilesetElement->UnsignedAttribute("tileheight", 0);
+      uint32_t tileWidth = tilesetElement->UnsignedAttribute("tilewidth", 0);
+      const char* nameCStr = tilesetElement->Attribute("name");
+
+      uint32_t imageWidth = 0;
+      uint32_t imageHeight = 0;
+      const char* imageSource = nullptr;
+
+      XMLElement* imageElement = tilesetElement->FirstChildElement("image");
+      if (imageElement)
+      {
+        imageWidth = imageElement->UnsignedAttribute("width", 0);
+        imageHeight = imageElement->UnsignedAttribute("height", 0);
+        imageSource = imageElement->Attribute("source");
+      }
+
+      return new SpriteSheetTileSet(
+        firstGid,
+        columns,
+        margin,
+        spacing,
+        tileHeight,
+        tileWidth,
+        tileCount,
+        imageWidth,
+        imageHeight,
+        imageSource ? imageSource : "",
+        nameCStr ? nameCStr : ""
+      );
+    }
+
+    TileSet* parseImageCollectionTileSet(
+      XMLElement* tilesetElement,
+      const int32_t firstGid
+    )
+    {
+      int32_t margin = tilesetElement->IntAttribute("margin", 0);
+      uint32_t spacing = tilesetElement->UnsignedAttribute("spacing", 0);
+      uint32_t columns = tilesetElement->UnsignedAttribute("columns", 0);
+      size_t tileCount = static_cast<size_t>(tilesetElement->IntAttribute("tilecount", 0));
+      uint32_t tileHeight = tilesetElement->UnsignedAttribute("tileheight", 0);
+      uint32_t tileWidth = tilesetElement->UnsignedAttribute("tilewidth", 0);
+      const char* nameCStr = tilesetElement->Attribute("name");
+
+      TileSetGrid* tileSetGrid = parseTileSetGrid(
+        tilesetElement->FirstChildElement("grid")
+      );
+
+      TileSetTile** tiles = parseTileSetTileArray(tilesetElement, tileCount);
+
+      ImageCollectionTileSet* imageCollectionSet = new ImageCollectionTileSet(
+        firstGid,
+        *tileSetGrid,
+        tiles,
+        tileCount,
+        columns,
+        margin,
+        spacing,
+        tileHeight,
+        tileWidth,
+        nameCStr ? nameCStr : ""
+      );
+
+      delete tileSetGrid;
+      return imageCollectionSet;
+    }
+
+    TileSetGrid* parseTileSetGrid(XMLElement* gridElement)
+    {
+      if (!gridElement)
+        return nullptr;
+
+      int32_t width = gridElement->IntAttribute("width", 0);
+      int32_t height = gridElement->IntAttribute("height", 0);
+
+      const char* orientationStr = gridElement->Attribute("orientation");
+      orientation::Type orientation = orientation::Type::Orthogonal;
+      if (orientationStr)
+        orientation = orientationParser::parseFromString(orientationStr);
+
+      return new TileSetGrid(width, height, orientation);
+    }
+
+    TileSetTile** parseTileSetTileArray(
+      XMLElement* tilesetElement,
+      size_t tileCount
+    )
+    {
+      TileSetTile** tiles = new TileSetTile * [tileCount];
+
+      size_t i = 0;
+      XMLElement* tileElement = tilesetElement->FirstChildElement("tile");
+      while (tileElement)
+      {
+        if (i >= tileCount)
+          break;
+
+        tiles[i] = parseTileSetTile(tileElement);
+
+        tileElement = tileElement->NextSiblingElement("tile");
+        ++i;
+      }
+
+      return tiles;
+    }
+
+    TileSetTile* parseTileSetTile(XMLElement* tileElement)
+    {
+      uint32_t id = tileElement->UnsignedAttribute("id", 0);
+
+      XMLElement* imageElement = tileElement->FirstChildElement("image");
+      if (imageElement)
+      {
+        uint32_t imageWidth = imageElement->UnsignedAttribute("width", 0);
+        uint32_t imageHeight = imageElement->UnsignedAttribute("height", 0);
+        const char* imageSource = imageElement->Attribute("source");
+        
+        return new TileSetTile(
+          id,
+          imageWidth,
+          imageHeight,
+          imageSource ? imageSource : ""
+        );
+      }
+
+      return nullptr;
+    }
+  };
+}
